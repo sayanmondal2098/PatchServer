636242a77eb8b2210b787204672c7c9157771cd0
Sayan Mondal <sayanmondal2098@gmail.com>
2021-12-24 00:42:01 +0530

    basic ws server added, able to handle large/ very large data

--- api/websocketConnection.js
+++ api/websocketConnection.js
@@ -1,8 +1,19 @@
 const WebSocket = require('ws');
-const files = require('fs');
+// const http = require('http');
+// const files = require('fs');
+
+
 
 const wss = new WebSocket.Server({
     port: 5002
 }, function () {
-    console.log('WebSocket Server is running on port 8080');
+    wss.on('connection', function connection(ws) {  
+        ws.on('message', function incoming(message) {
+            console.log('received: %s', message);
+        });
+        ws.send('something');
+    });
+    console.log('WebSocket Server is running on port 5002');
 });
+
+console.log(wss);
